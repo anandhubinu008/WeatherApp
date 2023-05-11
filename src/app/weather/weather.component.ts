baee3e7f32f068aebcff661b33a133f8e9df64e6
anandhubinu008 <postboxnumber0008@gmail.com>
2023-05-11 12:35:16 +0530

    chnages2

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,13 +10,19 @@ export class WeatherComponent implements OnInit{
   city :string = '';
   myWeather : any;
   temprature : number = 0;
-  tempMax : string = '';
-  tempMin :string = '';
-  services : string ='';
+  tempMax : string = '0';
+  tempMin :string = '0';
+  services : string ='----';
   units : string = 'metric';
-  cityNameValue: string = '';
-  humidity:string ='';
-  windSpeed:string =''; 
+  cityNameValue: string = '----';
+  humidity:string ='0';
+  windSpeed:string ='0'; 
+  morningImage = './assets/night.jpg';
+  dayImage ='./assets/pic2.jpg';
+  eveningImage = './assets/pic2.jpg';
+  nightImage = './assets/pic2.jpg';
+  date = new Date();
+  hours = new Date().getHours();
   cityName(eventData:Event){
     this.cityNameValue = ((<HTMLInputElement>eventData.target).value);
       }
@@ -33,7 +39,7 @@ export class WeatherComponent implements OnInit{
           this.myWeather = res;
           console.log(this.myWeather);
           this.temprature = this.myWeather.main.temp;
-          this.services = this.myWeather.weather[0].description;
+          this.services = this.myWeather.weather[0].main;
           this.tempMax = this.myWeather.main.temp_max;
           this.tempMin = this.myWeather.main.temp_min;
           this.humidity = this.myWeather.main.humidity;
@@ -41,7 +47,24 @@ export class WeatherComponent implements OnInit{
         },
   error :(error) => console.log(error.message),
   complete:() => console.info('API call complicated')
-        })
+        })     
+        setInterval(() => {         //replaced function() by ()=>
+          this.date = new Date();
+        }, 1000);
+  }
+  getTimeOfDay(): string {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 6 && currentHour < 12) {
+      return 'morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return 'day';
+    } else if (currentHour >= 18 && currentHour < 22) {
+      return 'evening';
+    } else {
+      return 'night';
+    }
+    
   }
 
 }
+
